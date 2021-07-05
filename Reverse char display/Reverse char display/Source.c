e62ad6f4034d8083f9f409d93c0f2456943d19c4
Domaoggy <85707302+Domaoggy@users.noreply.github.com>
2021-07-05 15:37:29 +0300

    Add files via upload

---
+++ Reverse char display/Reverse char display/Source.c
@@ -0,0 +1,16 @@
+/*Display char in reverse for MOD c*/
+#include <stdio.h> 
+
+int main()
+{
+    char char1 = 'A';
+    char char2 = 'M';
+    char char3 = 'O';
+
+
+    printf("The reverse of %c%c%c is %c%c%c\n",
+        char1, char2, char3,
+        char3, char2, char1);
+
+    return(0);
+}
